a23ceb67da32906288af8cd06fb516e9695f4346
Weliton Silva <welitonjosedasilva@gmail.com>
2025-02-05 08:16:00 -0300

    listas part ll

--- lista.py
+++ lista.py
@@ -11,3 +11,32 @@ print(minha_lista[5])
 print(minha_lista[1:7])
 print(minha_lista[:6])
 print(minha_lista[2:])
+
+#Métodos de listas
+
+#Método append(): adicona um elemento no final da lista
+minha_lista.append(6)
+print("Após append(6):", minha_lista)
+
+#Método index
+
+indice = minha_lista.index(6)
+print("Indice do elemento 6", indice)
+
+#Método insert: Insere um elemento em um indice especifico
+minha_lista.insert(2, 10)
+print("Após o insert (2,10):", minha_lista)
+
+# Método pop
+elemento_removido = minha_lista.pop(3)
+print("Elemento removido:", elemento_removido)
+print("Após o pop (3)", minha_lista)
+
+#Método remove
+
+minha_lista.remove(False)
+print("Após remove(False):", minha_lista)
+
+# Método sort
+#minha_lista.sort()
+#print("Após sort()", minha_lista)
