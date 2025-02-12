7e90cc4305801132e69037337d174259bc50363a
Weliton Silva <welitonjosedasilva@gmail.com>
2025-02-12 11:54:41 -0300

    for, range e enumerate

--- tipos_variaveis.py/for.py
+++ tipos_variaveis.py/for.py
@@ -23,3 +23,28 @@ print("\nFor Utilizando dicionario - valores")
 
 for chave, valor in pessoa.items():
     print(f"{chave}: {valor}")
+
+# range(): intervalo numérico
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+#print(list(range(0,10)))
+print("\nUtilizando a função range()")
+for numero in range(5):
+    print("Número",numero)
+
+print("\nUtilizando a função range() com len()")
+lista = [1, 2, 3, 4, 5]
+for indice in range(0, len(lista)):
+    print("indice: ", indice)
+    print("Elemento:", lista [indice])
+    if indice == 3:
+        lista [indice] = 5
+    else:
+        lista[indice] = 0
+print(lista)
+
+# enumerate()
+lista_enumerate = ["a", "b", "c"]
+for indice, valor in enumerate(lista):
+    print(f"{indice}: {valor}")
+    if indice == 1:
+        print("indice 1")
