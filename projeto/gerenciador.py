32176ec8559d19b063103639c63302799a0036cb
Weliton Silva <welitonjosedasilva@gmail.com>
2025-03-11 09:03:43 -0300

    finalizando gerenciador de tarefas

--- projeto/gerenciador.py
+++ projeto/gerenciador.py
@@ -25,6 +25,19 @@ def atualizar_nome_tarefa(tarefas,indice_tarefa, novo_nome_tarefa):
         print("Indice de tarefa inválido.")
     return
 
+def completar_tarefa (tarefas, indice_tarefa):
+    indice_tarefa_ajustado = int(indice_tarefa) -1
+    tarefas[indice_tarefa_ajustado]["completada"] = True
+    print(f"Tarefa {indice_tarefa} marcada como completada")
+    return
+
+def deletar_tarefas_completadas(tarefas):
+    for tarefa in tarefas:
+        if tarefa["completada"]:
+            tarefas.remove(tarefa)
+    print("Tarefas completadas foram deletadas")
+    return
+
 
 tarefas = []
 while True:
@@ -48,6 +61,13 @@ while True:
         indice_tarefa = input("Digite o número da tarefa que deseja atualizar: ")
         novo_nome = input("Digite o novo nome da tarefa: ")
         atualizar_nome_tarefa(tarefas, indice_tarefa, novo_nome)
+    elif escolha == "4":
+        ver_tarefas(tarefas)
+        indice_tarefa = input("Digite o número da tarefa que deseja completar: ")
+        completar_tarefa(tarefas, indice_tarefa)
+    elif escolha == "5":
+        deletar_tarefas_completadas(tarefas)
+        ver_tarefas(tarefas)
     elif escolha == "6":
         break
 
