8e3cf8b64b5a84c869226d6a33538f2a1e3225d4
Weliton Silva <welitonjosedasilva@gmail.com>
2025-03-19 08:37:48 -0300

    class

---
+++ MODULO_2/poo.py
@@ -0,0 +1,11 @@
+# POO
+
+# Classe exemplo
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def saudacao():
+        return(f' Ola, meu nome é {self.nome} e eu tenho {self.idade} anos.')
+    
